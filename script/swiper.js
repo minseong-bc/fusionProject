e2059dd2f3f0ee949bc7359b688ca3ceb0da9f46
minseong <alstjd426@365.bc.ac.kr>
2024-10-06 22:00:59 +0900

    Create Project Folder

---
+++ script/swiper.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
