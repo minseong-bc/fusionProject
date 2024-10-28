f8fcb447d64373ecaf84e33e6b9239972287b29d
minseong <alstjd426@365.bc.ac.kr>
2024-10-28 22:12:21 +0900

    express routing 완료

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var sanitizeHtml = require('sanitize-html');
+// pm2 start main.js --watch --ignore-watch="data/*" --no-daemon
+const fs = require('fs');
+const template = require('./lib/template.js');
+const sanitizeHtml = require('sanitize-html');
+const path = require('path');
+const qs = require('querystring');
+const express = require('express');
+const app = express();
+const port = 3000;
 const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
@@ -20,72 +22,104 @@ const pool = mysql.createPool({
   keepAliveInitialDelay: 0,
 });
  
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
 
-    if(pathname === '/'){
-      if(queryData.id === undefined){ // 메인 페이지 라면 => 메인페이지 출력
-        var title = 'main';
-        fs.readFile(`data/${title}`, 'utf8', (err, body) => {
-          var sanitizeBody = sanitizeHtml(body);       
-          var html = template.HTML(title, sanitizeBody);
-          response.writeHead(200);
-          response.end(html);
-        });
-      } else { // 그 외 페이지 출력
-        // var filiterId = path.parse(queryData.id).base; !! sanitizeHtml로 대체 !!
-        var sanitizedTitle = sanitizeHtml(path.parse(queryData.id).base);
-        fs.readFile(`data/${sanitizedTitle}`, 'utf8', (err, body) => { //1차 페이지 (지역, 축제, 식당)
-          var sanitizeBody = sanitizeHtml(body);      
-          var html = template.HTML(sanitizedTitle, sanitizeBody);
-          response.writeHead(200);
-          response.end(html);
-        });
-      }
-    } else if(pathname === '/register'){ // 회원가입 페이지
-      var title = 'register';
-        var html = template.HTML(title, `
-          <form action="http://localhost:3000/signup" method = "POST">
-            <input class="inputbox" id="id" name="id" type="text" placeholder="아이디를 입력해 주세요."/>
-            <input class="inputbox" id="name" name="name" type="text" placeholder="이름을 입력해 주세요."/>
-            <input class="inputbox" id="password1" name="password1" type="password" placeholder="비밀번호를 입력해 주세요."/>
-            <input class="phoneNum" id="phone1" name="phone1" type="text" maxlength="13" placeholder="전화번호 13자리를 입력해 주세요." />
-            <button type="submit" id="signup_button" onclick="signup()" >가입하기</button>
-        </form>
-        `);
-        response.writeHead(200);
-        response.end(html);
-      } else if(pathname === '/signup'){ // 회원가입 처리 
-        var body = '';
-        request.on('data', function(data){ // post 방식으로 데이터를 받을 때
-          body = body + data;
-        });
-        request.on('end', function(){ // 데이터를 다 받았을 때
-          var post = qs.parse(body);
-          var id = post.id;
-          var name = post.name;
-          var password1 = post.password1;
-          var phone1 = post.phone1;
-          /* 데이터베이스에 저장하는 코드가 와야함 */ 
-          // 리다이렉션 로그인 페이지로
-          // console.log(post);
-          response.writeHead(302, {Location: '/login'}); // 로그인 페이지로 리다이렉션(302)
-          response.end();
-        }); 
-      } else if(pathname === '/login'){ // 로그인 페이지
-        var title = 'login';
-        var html = template.HTML(title, `
-          // 폼태그 
-          `);
-          response.writeHead(200);
-          response.end(html);
-      }
-    else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
-
+app.get('/', (request, response) => { // 메인 페이지
+  var title = 'main';
+    fs.readFile(`data/${title}`, 'utf8', (err, body) => {
+      var sanitizeBody = sanitizeHtml(body);       
+      var html = template.HTML(title, sanitizeBody);
+      response.send(html);
+    });
 });
-app.listen(3000);
+
+app.get('/page/:pageId', (request, response) => { //세부 페이지 (지역, 축제, 식당) (회원가입 로그인 페이지 추가~~~~)
+  var filiterId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filiterId}`, 'utf8', (err, body) => { 
+      var sanitizedTitle = sanitizeHtml(filiterId);
+      var sanitizeBody = sanitizeHtml(body);      
+      var html = template.HTML(sanitizedTitle, sanitizeBody);
+      response.send(html);
+    });
+});
+
+app.get('/register', (request, response) => { // 회원가입 페이지
+  var title = 'register';
+  fs.readFile(`data/${title}`, 'utf8', (err, body) => {
+    var sanitizeBody = sanitizeHtml(body,{
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat(['form', 'input', 'button']),
+      allowedAttributes: {
+        '*': ['id', 'class', 'type', 'name', 'placeholder', 'value', 'onclick', 'maxlength']
+      }
+    });       
+    var html = template.register(title, sanitizeBody);
+    response.send(html);
+  });
+});
+
+app.post('/register', (request, response) => { // 회원가입 처리 라우팅
+  var body = '';
+  request.on('data', function(data){ // post 방식으로 데이터를 받을 때
+    body = body + data;
+  });
+  request.on('end', function(){ // 데이터를 다 받았을 때
+    var post = qs.parse(body);
+    var id = post.id;
+    var name = post.name;
+    var password = post.password1;
+    var phone = post.phone1;
+    pool.query(`INSERT INTO db_test.user VALUES (?, ?, ?, ?); `,[id, name, password, phone], (err, rows, fields) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("register success!");
+    }); 
+    response.redirect('/login');
+  }); 
+});
+
+app.get('/login', (request, response) => { // 로그인 페이지
+  var title = 'login';
+  fs.readFile(`data/${title}`, 'utf8', (err, body) => {
+    var sanitizeBody = sanitizeHtml(body,{
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat(['form', 'input', 'button']),
+      allowedAttributes: {
+        '*': ['id', 'class', 'type', 'name', 'placeholder', 'value', 'onclick'],
+      }
+    });       
+    var html = template.login(title, sanitizeBody);
+    response.send(html);
+  });
+});
+
+app.post('/login', (request, response) => { // 로그인 처리 라우팅
+  var body = '';
+  request.on('data', function(data){ // post 방식으로 데이터를 받을 때
+    body = body + data;
+  });
+  request.on('end', function(){ // 데이터를 다 받았을 때
+    var post = qs.parse(body);
+    var id = post.id;
+    var password = post.password;
+    
+    pool.query(`SELECT * FROM db_test.user WHERE id = ? and password = ?; `,[id, password], (err, rows, fields) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(rows[0].name + "님 환영합니다!");
+    }); 
+    response.redirect('/');
+  });
+});
+
+app.post('/', (request, response) => { // 메인 페이지
+  var title = 'main';
+    fs.readFile(`data/${title}`, 'utf8', (err, body) => {
+      var sanitizeBody = sanitizeHtml(body);       
+      var html = template.HTML(title, sanitizeBody);
+      response.send(html);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
